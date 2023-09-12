54ad73318fe0c0d01916d88c912f3532cd1322ae
Glitch (pushy-karts-dev) <none>
2023-09-12 21:44:43 +0000

    Physics based car hits

--- audio.js
+++ audio.js
@@ -1,20 +1,22 @@
 const audio = {};
 audio.context = new AudioContext();
-// audio.on = true;
+audio.on = true;
 
 const path = 'https://cdn.glitch.global/0a4c92ea-651d-46ad-9395-99ef6a57e497/';
 
 // All audio.sounds were generated online at https://www.leshylabs.com/apps/sfMaker/
 audio.sounds = {
-  boxhit: { audio: new Audio(path + "boxhit.wav"), volume: 0.01 },
-  carhit: { audio: new Audio(path + "carhit.wav"), volume: 0.1 },
+  boxhit: { audio: new Audio(path + 'boxhit.wav'), volume: 0.1 },
+  carhit: { audio: new Audio(path + 'carhit.wav'), volume: 1 },
 };
 
-audio.play = function (sound) {
+audio.play = function (sound, vol) {
   if(!this.on)
     return;
-  const snd = audio.sounds[sound].audio.cloneNode(true);
-  snd.volume = audio.sounds[sound].volume;
+  if(vol === 0)
+    vol = 1;
+  const snd = audio.sounds[sound].audio.cloneNode();
+  snd.volume = audio.sounds[sound].volume * vol;
   snd.play();
 };
 
